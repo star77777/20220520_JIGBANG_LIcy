0d5cbde0838f16ebfb36ae13c391dcd6b0737786
star77777 <104746184+star77777@users.noreply.github.com>
2022-05-20 10:48:46 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a20220520_jigbang_licy/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a20220520_jigbang_licy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
